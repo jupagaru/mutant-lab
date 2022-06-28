fde3fc7e23d784ddf02f5c65431d687f2c87efde
jupagaru <jupagaru@gmail.com>
2022-06-27 21:46:20 -0500

    Se cambia el tipo de dato del campo isMutant. Se agrega prueba de integración de modificación

--- src/test/java/com/mercadolibre/mutan/repository/MutantRepositoryIT.java
+++ src/test/java/com/mercadolibre/mutan/repository/MutantRepositoryIT.java
@@ -3,6 +3,7 @@ package com.mercadolibre.mutan.repository;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,25 +19,45 @@ class MutantRepositoryIT {
 	MutantRepository mutantRepository;
 
 	@Test
+	@Order(1)
 	void debeValidarLasDependencias() {
 		assertNotNull(mutantRepository);
 	}
 	
 	@Test
-	void deveCrearRegistroMutant() {
+	@Order(2)
+	void debeCrearRegistroMutant() {
 		//Arrange
 		Integer mutantId = 1234;
 		
 		Mutant mutant = new Mutant();
 		mutant.setMutantId(mutantId);
 		mutant.setAdn("ATGCGA,CAGTGC,TTATGT,AGAAGG,CCCCTA,TCACTG");
-		mutant.setMutant(true);
+		mutant.setIsMutant("S");
 		
 		//Act
 		mutantRepository.save(mutant);
 		
 		//Assert
 		assertNotNull(mutant, "El mutante es nulo, no se pudo grabar");
+	}
+	
+	@Test
+	@Order(3)
+	void debeModificarRegistroMutant() {
+		//Arrange
+		Integer mutantId = 1234;
+		Mutant mutant = null;
+		
+		mutant = mutantRepository.findById(mutantId).get();
+		System.out.println("encontró mutant "+ mutant.getAdn());
+		mutant.setIsMutant("N");
+		
+		//Act
+		mutantRepository.save(mutant);
+		
+		//Assert
+		assertNotNull(mutant, "El mutante es nulo, no se pudo modificar");
 	}
 
 }
