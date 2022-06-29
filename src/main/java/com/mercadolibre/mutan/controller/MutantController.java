22882d3c965a617c3f4c0faf16cb6101e36764e9
jupagaru <jupagaru@gmail.com>
2022-06-28 22:28:39 -0500

    Se crean los métodos findAll y findById en el  controller

--- src/main/java/com/mercadolibre/mutan/controller/MutantController.java
+++ src/main/java/com/mercadolibre/mutan/controller/MutantController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mercadolibre.mutan.domain.Mutant;
 import com.mercadolibre.mutan.dto.MutantDTO;
+import com.mercadolibre.mutan.mapper.MutantMapper;
 import com.mercadolibre.mutan.service.MutantService;
 
 @RestController
@@ -17,9 +20,22 @@ public class MutantController {
 	@Autowired
 	MutantService mutantService;
 	
+	@Autowired
+	MutantMapper mutantMapper;
+	
+	
+	@GetMapping("/{mutantId}")
+	public MutantDTO findById(@PathVariable("mutantId") Integer  mutantId) throws Exception{
+		Mutant mutant = (mutantService.findById(mutantId).isPresent()==true) ? mutantService.findById(mutantId).get():null;
+		return mutantMapper.mutantToMutantDTO(mutant);
+	}
 	@GetMapping()
 	public List<MutantDTO> findAll() throws Exception{
-		return null;
+		
+		List<Mutant> mutants = mutantService.findAll();
+		List<MutantDTO> mutantDTOs = mutantMapper.mutantListToMutantDTOList(mutants);
+		
+		return mutantDTOs;
 	}
 
 }
