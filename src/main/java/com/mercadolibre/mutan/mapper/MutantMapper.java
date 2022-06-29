337d0fb2575889af0b889717a772332a2998abe7
jupagaru <jupagaru@gmail.com>
2022-06-28 22:15:52 -0500

    Se mapean listas para pasar del dominio al dto y dto al dominio

--- src/main/java/com/mercadolibre/mutan/mapper/MutantMapper.java
+++ src/main/java/com/mercadolibre/mutan/mapper/MutantMapper.java
@@ -1,5 +1,7 @@
 package com.mercadolibre.mutan.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 
 import com.mercadolibre.mutan.domain.Mutant;
@@ -11,5 +13,9 @@ public interface MutantMapper {
 	public MutantDTO mutantToMutantDTO(Mutant mutant);
 	
 	public Mutant mutantDTOToMutant(MutantDTO mutantDTO);
+	
+	public List<MutantDTO> mutantListToMutantDTOList(List<Mutant> mutants);
+	
+	public List<Mutant> mutantDTOListToMutantList(List<MutantDTO> mutantDTOs);
 
 }
