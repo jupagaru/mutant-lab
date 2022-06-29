f82c32882beee56cce9b85224aafc39f8496ec96
jupagaru <jupagaru@gmail.com>
2022-06-28 22:11:26 -0500

    Se mapea la información de Mutant a MutantDTO

--- src/main/java/com/mercadolibre/mutan/mapper/MutantMapper.java
+++ src/main/java/com/mercadolibre/mutan/mapper/MutantMapper.java
@@ -1,5 +1,13 @@
 package com.mercadolibre.mutan.mapper;
 
+import org.mapstruct.Mapper;
+
+import com.mercadolibre.mutan.domain.Mutant;
+import com.mercadolibre.mutan.dto.MutantDTO;
+
+@Mapper
 public interface MutantMapper {
+	
+	public MutantDTO mutantToMutantDTO(Mutant mutant);
 
 }
